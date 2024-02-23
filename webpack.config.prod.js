552632c8717d3a2748c902c627ab5fcadeae13bb
Dakota <dakotacustompcs@gmail.com>
2024-02-23 03:16:01 -0500

    Generate dynamic HTML for our website in the dist directory.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,21 @@
 import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
   mode: "production",
   devtool: "source-map",
-  entry: "./src/index.js",
+  entry: "./src/index.html",
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
     filename: "bundle.js",
   },
-  plugins: [],
+  plugins: [
+    // Create HTML File that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
